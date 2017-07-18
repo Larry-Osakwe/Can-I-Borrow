68669d4a482f7e1864d2346eff2e4bca004f4aff
Larry <larryosak@gmail.com>
2017-07-17 20:09:58 -0700

    Create base UI with styles and colors

---
+++ app/src/main/java/com/larry/osakwe/caniborrow/EditorActivity.java
@@ -0,0 +1,62 @@
+package com.larry.osakwe.caniborrow;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+/**
+ * Allows user to create a new item or edit an existing one.
+ */
+public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the item's name */
+    private EditText mNameEditText;
+
+    /** EditText field to enter the borrower's name */
+    private EditText mBorrowerEditText;
+
+    /** EditText field to enter the return date */
+    private Spinner mDateSpinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
