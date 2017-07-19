2f29417af24ea311be4bebaaae773106edf06c31
Larry <larryosak@gmail.com>
2017-07-18 20:45:34 -0700

    Update CatalogActivity layout

--- app/src/main/java/com/larry/osakwe/caniborrow/data/ItemContract.java
+++ app/src/main/java/com/larry/osakwe/caniborrow/data/ItemContract.java
@@ -4,12 +4,18 @@ package com.larry.osakwe.caniborrow.data;
  * Created by Larry Osakwe on 7/18/2017.
  */
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * API Contract for the Borrow app.
  **/
 public class ItemContract {
+
+    public static final String CONTENT_AUTHORITY = "com.larry.osakwe.caniborrow";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_ITEMS = "items";
 
     // To prevent someone from accidentally instantiating the contract class, give
     // it an empty constructor
@@ -21,6 +27,9 @@ public class ItemContract {
      * Each entry in the table represents a single item.
      */
     public static final class ItemEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_ITEMS);
+
         /**
          * Name of database table for items
          */
@@ -56,6 +65,13 @@ public class ItemContract {
          */
 
         public final static String COLUMN_DATE_TIME = "date";
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
+                "/" + CONTENT_AUTHORITY + "/" + PATH_ITEMS;
+
 
     }
 }
